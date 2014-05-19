006dbc3013636575527c7586463049aa531d96ea
Mehade <Mehade@MehadeHasan>
2014-05-19 22:22:06 +0600

    Merge origin/master
    Conflicts:
    	index.php
    	nbproject/project.xml

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'calla2z_sdl1';
-$db['default']['password'] = 'rcis12345..';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
 $db['default']['database'] = 'calla2z_sdl1';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
