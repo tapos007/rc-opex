ca88a292b084682aa345a302c720a3978c8e06dc
Imtiaj Ahmed <Imtiaj Ahmed@Imtiaj>
2014-06-05 04:50:21 +0600

    Merge origin/master
    Conflicts:
    	application/controllers/con_proc_monthly_report_generate.php
    	application/controllers/con_set_worker_profile.php
    	application/models/mod_monthly_wages_detail.php
    	application/models/mod_set_employee_salary.php
    	application/views/temp/worker_profile/edit.php

--- application/controllers/con_proc_daily_report_generate.php
+++ application/controllers/con_proc_daily_report_generate.php
@@ -342,12 +342,12 @@ class Con_proc_daily_report_generate extends CI_Controller {
         return $tbl_access_log_raw;
     }
 
-    public function pull_data_from_access_log() {
+    public function pull_data_from_access_log($today) {
         date_default_timezone_set('Asia/Dacca');
-        $today = date('Y-m-d', now());
+        //$today = date('Y-m-d', now());
         $yesterday = date('Y-m-d', strtotime('-1 day', strtotime($today)));
         echo $today . '<br/>' . $yesterday;
-        //exit();
+        exit();
         //populate tbl_access_log
         $tbl_access_log_raw = $this->mod_access_log->get_floor_specific_access_record($today);
         echo count($tbl_access_log_raw);
